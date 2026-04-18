7f44e8129790b9148989bd75718e9b32d272e294
muhamad jaelani <jaelanim465@gmail.com>
2026-04-18 16:36:50 +0700

    Add Product and User model definitions

---
+++ internal/model/product.go
@@ -0,0 +1,9 @@
+package model
+
+type Product struct {
+	ID          uint   `gorm:"primaryKey"`
+	Name        string `gorm:"not null"`
+	Description string
+	Price       float64 `gorm:"not null"`
+	Stock       int     `gorm:"not null"`
+}
---
+++ internal/model/user.go
@@ -0,0 +1,8 @@
+package model
+
+type User struct {
+	ID       uint   `gorm:"primaryKey"`
+	Name     string `gorm:"not null"`
+	Email    string `gorm:"unique;not null"`
+	Password string `gorm:"not null"`
+}
